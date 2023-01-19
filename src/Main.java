efb20777e881af032994fc6933ff35bc3a67d01f
Dmitry <a89297175356@gmail.com>
2023-01-19 16:54:17 +0500

    Priveryai

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
+    public class Rrr{
+        void www() {
+
+        }
+    }
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        System.out.println("Hello world!2");
+
     }
 }
